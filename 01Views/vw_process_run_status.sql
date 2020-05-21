0f5c3b4ccbbd1525e38769f09b63b4efa846d335
Vikram Krishnamurthy <62871855+vikramkrishnamurthy@users.noreply.github.com>
2020-05-21 15:41:15 +0530

    Add files via upload

---
+++ 01Views/vw_process_run_status.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE VIEW EDW_NORM.vw_process_run_status
+ AS
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--+ View Name:        vw_process_run_status
+--+ Created By:       Deepak Shenoy
+--+ Created Date:     02/05/2020
+--+
+--+ Description
+--+ ----------------------------------------------------------------------------
+--+ This view will provide realtime/active run status of the process object
+--+
+--+ Modification History
+--+ ----------------------------------------------------------------------------
+--+ Date        Release       Modified By       Comments
+--+ 02/05/2020  Poseidon 0.8  Deepak Shenoy     Initial Version
+--+
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SELECT cnf.schema_name
+      ,cnf.sp_name
+      ,cnf.table_name
+      ,adt.row_id run_id
+      ,adt.start_dtm
+  FROM EDW_NORM.process_audit adt
+      ,EDW_NORM.process_config cnf
+ WHERE adt.config_id = cnf.config_id
+   AND adt.run_status = 'R' ;
